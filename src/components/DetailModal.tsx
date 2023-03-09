4f4459c1d2116c961eb3e8a8214f84cafa5cfd33
hyemin33 <124856203+hyemin33@users.noreply.github.com>
2023-03-09 22:25:25 +0900

    [#14] 모달 만들기  (#17)
    * feat: detailModal 추가
    
    * fix: main에 modal 샘플로 연결, modal style 수정

---
+++ src/components/DetailModal.tsx
@@ -0,0 +1,84 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Text,
+  Divider,
+  Box,
+  Badge,
+  Stack,
+  Image,
+  Flex,
+} from '@chakra-ui/react';
+import Product from '@src/types/Product';
+
+interface DetailModalProps {
+  onClose: () => void;
+  isOpen: boolean;
+  detailData: Product;
+}
+
+const DetailModal = ({ onClose, isOpen, detailData }: DetailModalProps) => {
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} size='xl' scrollBehavior='inside'>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader />
+        <ModalCloseButton />
+        <ModalBody>
+          <Stack spacing='4px' mb='10px'>
+            <Text fontSize='sm' as='b' color='gray.500'>
+              대한민국 &gt; {detailData?.spaceCategory}
+            </Text>
+            <Text fontSize='xl' as='b'>
+              {detailData?.name}
+            </Text>
+            <Text fontSize='sm' color='gray.700' textAlign='right'>
+              {detailData?.registrationDate}
+            </Text>
+          </Stack>
+          <Stack spacing='14px'>
+            <Box position='relative'>
+              <Image
+                src={detailData?.mainImage}
+                width={528}
+                height={300}
+                objectFit='cover'
+                alt={detailData?.name + '이미지'}
+              />
+              <Badge
+                colorScheme='blue'
+                position='absolute'
+                top='15px'
+                left='15px'
+                fontSize='16px'
+              >
+                #{detailData?.idx}
+              </Badge>
+            </Box>
+            <Flex direction='column'>
+              <Text fontSize='xl' color='red.700' textAlign='right'>
+                가격 {detailData?.price}원
+              </Text>
+              <Badge colorScheme='red' textAlign='right' ml='auto' mt='5px'>
+                최대 {detailData?.maximumPurchases}장 구매 가능
+              </Badge>
+            </Flex>
+            <Divider />
+            <Text>{detailData?.description}</Text>
+          </Stack>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>닫기</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default DetailModal;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,19 @@
-import { Text } from '@chakra-ui/react';
+import { Text, useDisclosure } from '@chakra-ui/react';
 import { useContext } from 'react';
 import { ProductsContext } from '@src/context/products';
+import DetailModal from '@src/components/DetailModal';
 
 export const MainPage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { products } = useContext(ProductsContext);
 
   console.log(products);
 
-  return <Text>메인 페이지 입니다.</Text>;
+  return (
+    <>
+      <Text>메인 페이지 입니다.</Text>
+      <button onClick={onOpen}>modal button</button>
+      <DetailModal detailData={products[0]} onClose={onClose} isOpen={isOpen} />
+    </>
+  );
 };
