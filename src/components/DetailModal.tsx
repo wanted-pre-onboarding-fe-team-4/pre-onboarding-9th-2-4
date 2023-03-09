320ba01d8b7cd6dba16a553cf7510e4e5ef81473
hyemin33 <joliie@naver.com>
2023-03-09 22:15:05 +0900

    fix: main에 modal 샘플로 연결, modal style 수정

--- src/components/DetailModal.tsx
+++ src/components/DetailModal.tsx
@@ -1,93 +1,84 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    Text,
-    Divider,
-    Box,
-    Badge,
-    Stack,
-    Image,
-    Flex,
-  } from "@chakra-ui/react";
-  
-  interface DetailModalProps {
-    onClose: () => void;
-    isOpen: boolean;
-    item: {
-      idx: number;
-      name: string;
-      mainImage: string;
-      description: string;
-      spaceCategory: string;
-      price: number;
-      maximumPurchases: number;
-      registrationDate: string;
-    };
-  }
-  
-  const ModalComponent = ({ onClose, isOpen, item }: DetailModalProps) => {
-    return (
-      <Modal onClose={onClose} isOpen={isOpen} size="xl" scrollBehavior="inside">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader />
-          <ModalCloseButton />
-          <ModalBody>
-            <Stack spacing="4px" mb="10px">
-              <Text fontSize="sm" as="b" color="gray.500">
-                대한민국 &gt; {item?.spaceCategory}
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Text,
+  Divider,
+  Box,
+  Badge,
+  Stack,
+  Image,
+  Flex,
+} from '@chakra-ui/react';
+import Product from '@src/types/Product';
+
+interface DetailModalProps {
+  onClose: () => void;
+  isOpen: boolean;
+  detailData: Product;
+}
+
+const DetailModal = ({ onClose, isOpen, detailData }: DetailModalProps) => {
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} size='xl' scrollBehavior='inside'>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader />
+        <ModalCloseButton />
+        <ModalBody>
+          <Stack spacing='4px' mb='10px'>
+            <Text fontSize='sm' as='b' color='gray.500'>
+              대한민국 &gt; {detailData?.spaceCategory}
+            </Text>
+            <Text fontSize='xl' as='b'>
+              {detailData?.name}
+            </Text>
+            <Text fontSize='sm' color='gray.700' textAlign='right'>
+              {detailData?.registrationDate}
+            </Text>
+          </Stack>
+          <Stack spacing='14px'>
+            <Box position='relative'>
+              <Image
+                src={detailData?.mainImage}
+                width={528}
+                height={300}
+                objectFit='cover'
+                alt={detailData?.name + '이미지'}
+              />
+              <Badge
+                colorScheme='blue'
+                position='absolute'
+                top='15px'
+                left='15px'
+                fontSize='16px'
+              >
+                #{detailData?.idx}
+              </Badge>
+            </Box>
+            <Flex direction='column'>
+              <Text fontSize='xl' color='red.700' textAlign='right'>
+                가격 {detailData?.price}원
               </Text>
-              <Text fontSize="xl" as="b">
-                {item?.name}
-              </Text>
-              <Text fontSize="sm" color="gray.700" textAlign="right">
-                {item?.registrationDate}
-              </Text>
-            </Stack>
-            <Stack spacing="14px">
-              <Box position="relative">
-                <Image
-                  src={item.mainImage}
-                  width={528}
-                  height={300}
-                  objectFit="cover"
-                  alt={item?.name + "이미지"}
-                />
-                <Badge
-                  colorScheme="purple"
-                  position="absolute"
-                  top="15px"
-                  left="15px"
-                  fontSize="16px"
-                >
-                  #{item?.idx}
-                </Badge>
-              </Box>
-              <Flex direction="column">
-                <Text fontSize="2xl" color="tomato" textAlign="right">
-                  가격 {item?.price}원
-                </Text>
-                <Badge colorScheme="red" textAlign="right" ml="auto" mt="5px">
-                  최대 {item?.maximumPurchases}장 구매 가능
-                </Badge>
-              </Flex>
-              <Divider />
-              <Text>{item?.description}</Text>
-            </Stack>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>닫기</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    );
-  };
-  
-  export default ModalComponent;
-  
+              <Badge colorScheme='red' textAlign='right' ml='auto' mt='5px'>
+                최대 {detailData?.maximumPurchases}장 구매 가능
+              </Badge>
+            </Flex>
+            <Divider />
+            <Text>{detailData?.description}</Text>
+          </Stack>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>닫기</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default DetailModal;
