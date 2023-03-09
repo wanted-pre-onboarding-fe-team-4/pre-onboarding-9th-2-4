30a57d4021eaef18ae9c6fd42711499ca1a56202
송경환 <as5427072@gmail.com>
2023-03-09 12:49:09 +0900

    [#5] 글로벌스타일, 테마 적용 (#6)
    * chore notosankr 추가
    
    * feat css 리셋, 글로벌 테마 적용
    
    * style 브랜드 컬러 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { theme } from './components/commons/styles/Theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <ChakraProvider resetCSS={true} theme={theme}>
+    <App />
+  </ChakraProvider>
 );
