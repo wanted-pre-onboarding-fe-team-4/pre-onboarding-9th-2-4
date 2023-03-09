701055e73c558bea96dc45e40142bb725fc20085
SongNoin <as5427072@gmail.com>
2023-03-09 11:52:24 +0900

    feat css 리셋, 글로벌 테마 적용

---
+++ src/components/commons/styles/Theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+import '@fontsource/noto-sans-kr';
+
+export const theme = extendTheme({
+  fonts: {
+    heading: 'noto-sans-kr',
+    body: 'noto-sans-kr',
+  },
+});
