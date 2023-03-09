8bec49199603e099b7eec88064e8f783330b8ef8
SongNoin <as5427072@gmail.com>
2023-03-09 12:02:28 +0900

    style 브랜드 컬러 추가

--- src/components/commons/styles/Theme.ts
+++ src/components/commons/styles/Theme.ts
@@ -2,6 +2,19 @@ import { extendTheme } from '@chakra-ui/react';
 import '@fontsource/noto-sans-kr';
 
 export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#BEE3F8',
+      200: '#90CDF4',
+      300: '#63B3ED',
+      400: '#4299E1',
+      500: '#3182CE',
+      600: '#2B6CB0',
+      700: '#2C5282',
+      800: '#2A4365',
+      900: '#1A365D',
+    },
+  },
   fonts: {
     heading: 'noto-sans-kr',
     body: 'noto-sans-kr',
