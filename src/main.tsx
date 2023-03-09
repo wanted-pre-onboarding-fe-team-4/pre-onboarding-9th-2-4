cc2fd39214e5523f50a3a4077c0a9def6de256d2
ws8313 <87023889+ws8313@users.noreply.github.com>
2023-03-09 21:58:16 +0900

    [#12] 여행 상품 정보 가져오기 (#15)
    * feat: Product 타입 추가
    
    * feat: 여행 상품 정보 가져오는 api 추가
    
    * feat: 여행 상품 정보 컨텍스트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { theme } from './components/commons/styles/Theme';
+import ProductsProvider from './context/products';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider resetCSS={true} theme={theme}>
-    <App />
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
   </ChakraProvider>
 );
