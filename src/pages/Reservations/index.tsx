af812955478ea0511cc2fa9f90e3149d2fe07592
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-10 21:57:22 +0900

    feat: add message when empty basket list

--- src/pages/Reservations/index.tsx
+++ src/pages/Reservations/index.tsx
@@ -13,15 +13,26 @@ export const ReservationPage = () => {
     (acc, cur) => acc + cur.price * cur.count,
     0
   );
+  console.log(cart.length);
 
   return (
     <>
-      <VStack mb={20}>
-        <Heading>예약 총액은 {cartValueSum.toLocaleString()}원 입니다.</Heading>
-      </VStack>
-      <Suspense fallback={<ProductLiSkeleton />}>
-        <ReservationList filterFunction={cartItemsFilter} />
-      </Suspense>
+      {cart.length ? (
+        <>
+          <VStack mb={20}>
+            <Heading>
+              예약 총액은 {cartValueSum.toLocaleString()}원 입니다.
+            </Heading>
+          </VStack>
+          <Suspense fallback={<ProductLiSkeleton />}>
+            <ReservationList filterFunction={cartItemsFilter} />
+          </Suspense>
+        </>
+      ) : (
+        <VStack mt={40}>
+          <Heading>장바구니에 아무것도 담겨있지 않습니다</Heading>
+        </VStack>
+      )}
     </>
   );
 };
