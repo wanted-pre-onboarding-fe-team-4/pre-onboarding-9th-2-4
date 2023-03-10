61dd3a877980d863709521720ed65149de975378
hyemin33 <124856203+hyemin33@users.noreply.github.com>
2023-03-10 22:33:03 +0900

    fix: layout 반응형 수정 (#59)

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Flex } from '@chakra-ui/react';
+import { Container, Grid, GridItem } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '@src/layouts/units/Header';
 
@@ -13,12 +13,10 @@ export const DefaultLayout = () => {
       <GridItem area='header'>
         <Header />
       </GridItem>
-      <GridItem area='main' p='20'>
-        <Flex justifyContent='center'>
-          <div style={{ maxWidth: '1200px' }}>
-            <Outlet />
-          </div>
-        </Flex>
+      <GridItem area='main'>
+        <Container maxW='1200px' minW='320px' p='80px 16px'>
+          <Outlet />
+        </Container>
       </GridItem>
     </Grid>
   );
