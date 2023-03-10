ccfb47e7b2fa41843f75082e3f863e6cacac2e9c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-10 11:12:16 +0900

    fix: use api in getProductList

--- src/api/getProductList.ts
+++ src/api/getProductList.ts
@@ -1,15 +1,11 @@
-import axios from 'axios';
-import { ProductType } from '@src/types';
-
-const API_URL =
-  'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f1be87a4-38e1-4c1e-a527-bd4775812374/mock_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230309%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230309T144204Z&X-Amz-Expires=86400&X-Amz-Signature=7ff59cffb0e5e9c75d99b59290c467f1d0d1a6915e8400323716239f3ea575b2&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22mock_data.json%22&x-id=GetObject';
+import { getProducts } from './api';
 const getProductList = async () => {
   await new Promise((resolve) => {
     setTimeout(resolve, 1000);
   });
 
   try {
-    const { data } = await axios.get<ProductType[]>(API_URL);
+    const data = await getProducts();
     return data;
   } catch (e) {
     return null;
