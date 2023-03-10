525f9009d59321e26d304a9c3eb2b256313fae69
ws8313 <87023889+ws8313@users.noreply.github.com>
2023-03-10 23:49:07 +0900

    feat: 필터링 된 목록이 없는 경우 안내 문구 추가 (#66)

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@chakra-ui/react';
+import { Grid, Heading, VStack } from '@chakra-ui/react';
 import Product from './Product';
 import createResource from '@src/lib/createResource';
 import getProductList from '@src/api/getProductList';
@@ -15,19 +15,29 @@ const ProductList = ({ filterFunction }: ProductListProps) => {
   const filteredProducts = products.filter(filterFunction);
 
   return (
-    <Grid
-      gridTemplateColumns={[
-        'repeat(1, 1fr)',
-        'repeat(2, 1fr)',
-        'repeat(3, 1fr)',
-        'repeat(4, 1fr)',
-      ]}
-      gap='2rem'
-    >
-      {filteredProducts.map((product) => (
-        <Product key={product.idx} product={product} />
-      ))}
-    </Grid>
+    <>
+      {filteredProducts.length !== 0 && (
+        <Grid
+          gridTemplateColumns={[
+            'repeat(1, 1fr)',
+            'repeat(2, 1fr)',
+            'repeat(3, 1fr)',
+            'repeat(4, 1fr)',
+          ]}
+          gap='2rem'
+        >
+          {filteredProducts.map((product) => (
+            <Product key={product.idx} product={product} />
+          ))}
+        </Grid>
+      )}
+
+      {filteredProducts.length === 0 && (
+        <VStack mt={40}>
+          <Heading>조건에 맞는 상품이 없습니다</Heading>
+        </VStack>
+      )}
+    </>
   );
 };
 
