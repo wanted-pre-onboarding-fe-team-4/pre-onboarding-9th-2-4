9edc57de3bc2f196db45f3c691d13ec098260989
Seongbin Kim <seongbin9786@gmail.com>
2023-03-10 17:23:18 +0900

    feat(Cart): Changed Cart Context to have its count too

--- src/context/cart.tsx
+++ src/context/cart.tsx
@@ -6,9 +6,15 @@ import React, {
   useState,
 } from 'react';
 
+interface CartItem {
+  idx: number;
+  count: number;
+}
+
 interface CartContextType {
-  cart: number[];
+  cart: CartItem[];
   addToCart: (p: number) => void;
+  updateCount: (idx: number, count: number) => void;
   removeFromCart: (p: number) => void;
 }
 
@@ -16,6 +22,8 @@ const initialCartContext: CartContextType = {
   cart: [],
   /* eslint-disable-next-line */
   addToCart: (p) => {},
+  /* eslint-disable-next-line */
+  updateCount: (idx, count) => {},
   /* eslint-disable-next-line */
   removeFromCart: (p) => {},
 };
@@ -26,19 +34,28 @@ const CART_KEY = 'cart';
 
 const initialCart = JSON.parse(localStorage.getItem(CART_KEY) || '[]');
 
-const persistCart = (cart: number[]) => {
+const persistCart = (cart: CartItem[]) => {
   localStorage.setItem(CART_KEY, JSON.stringify(cart));
 };
 
 export const CartProvider = (props: React.PropsWithChildren) => {
-  const [cart, updateCart] = useState<number[]>(initialCart);
+  const [cart, updateCart] = useState<CartItem[]>(initialCart);
+
+  const updateCount = (newProduct: number, newCount: number) => {
+    const newCartItem = {
+      idx: newProduct,
+      count: newCount,
+    };
+    removeFromCart(newProduct);
+    updateCart((cart) => [newCartItem, ...cart]);
+  };
 
   const addToCart = (newProduct: number) => {
-    updateCart((cart) => [newProduct, ...cart]);
+    updateCount(newProduct, 1);
   };
 
   const removeFromCart = (toRemove: number) => {
-    updateCart((cart) => cart.filter((product) => product !== toRemove));
+    updateCart((cart) => cart.filter((product) => product.idx !== toRemove));
   };
 
   // 영속화 수행
@@ -50,6 +67,7 @@ export const CartProvider = (props: React.PropsWithChildren) => {
     () => ({
       cart,
       addToCart,
+      updateCount,
       removeFromCart,
     }),
     [cart]
--- src/pages/Reservations/index.tsx
+++ src/pages/Reservations/index.tsx
@@ -1,18 +1,19 @@
 import { Suspense } from 'react';
-import ProductList from '@src/components/commons/ProductList';
 import ProductLiSkeleton from '@src/components/commons/ProductSkeleton';
 import { useCart } from '@src/context/cart';
 import { ProductType } from '@src/types';
+import { ReservationList } from '@src/components/commons/ReservationList';
 
 export const ReservationPage = () => {
   const { cart } = useCart();
   console.log(cart);
-  const cartItemsFilter = (p: ProductType) => cart.includes(p.idx);
+  const cartItemsFilter = (p: ProductType) =>
+    !!cart.find((cart) => cart.idx === p.idx);
 
   return (
     <>
       <Suspense fallback={<ProductLiSkeleton />}>
-        <ProductList filterFunction={cartItemsFilter} />
+        <ReservationList filterFunction={cartItemsFilter} />
       </Suspense>
     </>
   );
